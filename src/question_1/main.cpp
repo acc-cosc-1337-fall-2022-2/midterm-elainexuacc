5fc3f4365c9be200e8178a8385493cf972b114ef
elainexuacc <can.xu@g.austincc.edu>
2022-10-16 04:55:31 +0000

    question 1 with test cases

--- src/question_1/main.cpp
+++ src/question_1/main.cpp
@@ -1,6 +1,16 @@
+#include "question1.h"
+#include <iostream>
+
+using std::cout;
+using std::cin;
 
 int main()
 {
+    int sold;
+    cin>>sold;
 
-    return 0;
+    int points;
+    points = get_earned_points(sold);
+
+    cout<<"Points Earned "<<points<<"\n";
 }
